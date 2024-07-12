151ade2ad1c01ac6396fd3715f776894b11b1d66
AKSHITA KUKREJA <akshita.kukreja.ext@siemens-healthineers.com>
2024-07-12 13:21:22 +0530

    Added casing

--- Services/NagarroProblems.cs
+++ Services/NagarroProblems.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +23,14 @@ namespace Practise.Services
 
             Console.WriteLine(ReturnSecondLargestNumOfAnArray(3, [2, 2, 2])); // output -1
             Console.WriteLine(ReturnSecondLargestNumOfAnArray(3, [2, 1, 2])); // output 2
+
+            Console.WriteLine(ReturnCorrectVariableName("this_is_a_variable")); //thisIsAVariable
+            Console.WriteLine(ReturnCorrectVariableName("modify_VariableName")); //modifyVariableName
+            Console.WriteLine(ReturnCorrectVariableName("thisIsAVariable")); //this_is_a_variable
+            Console.WriteLine(ReturnCorrectVariableName("aNA")); //a_n_a
+            Console.WriteLine(ReturnCorrectVariableName("this_is_a_variable_v_")); //thisIsAVariableV
+
+
 
         }
 
@@ -48,9 +58,10 @@ namespace Practise.Services
         // If no such num exists, return -1
         public int ReturnNumThatOccursMoreThanHalfTimes(int numOfElements, int[] arrayOfIntegers)
         {
-            foreach(int num in arrayOfIntegers)
+            int half = numOfElements / 2; 
+            foreach (int num in arrayOfIntegers)
             {
-                if(arrayOfIntegers.Count(x => x == num) > numOfElements / 2) return num;
+                if(arrayOfIntegers.Count(x => x == num) > half) return num;
             }
             return -1;
 
@@ -58,7 +69,6 @@ namespace Practise.Services
 
 
         // Second Largest Number of Array
-
         public int ReturnSecondLargestNumOfAnArray(int numOfElements, int [] arrayOfNumbers)
         {
             var distintArray = arrayOfNumbers.Distinct().ToArray();
@@ -71,18 +81,55 @@ namespace Practise.Services
 
         }
 
+        /// <summary>
+        /// this_is_a_variable->thisIsAVariable
+        /// modify_VariableName->modifyVariableName
+        /// thisIsAVariable->this_is_a_variable
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        public string ReturnCorrectVariableName(string inputString)
+        {
+            int indexOfUpperCase = inputString.IndexOf(inputString.FirstOrDefault(x => char.IsUpper(x))) ;
+            int indexOfUnderScore = inputString.IndexOf('_');
+
+            string result = string.Empty;
+
+            indexOfUnderScore = indexOfUnderScore >= 0 ? indexOfUnderScore : int.MaxValue;
+            indexOfUpperCase = indexOfUpperCase >= 0 ? indexOfUpperCase : int.MaxValue;
+
+            int min = int.Min(indexOfUpperCase, indexOfUnderScore);
+
+            if(min == indexOfUpperCase) // convert to camel case -> thisIsAVariable->this_is_a_variable -> thisIsAN -> this_is_a_n
+            {
+                for(int i =0; i< inputString.Length; i++)
+                {
+                    result = char.IsUpper(inputString[i]) ? result + "_" + char.ToLower(inputString[i]) : result + inputString[i];
+                }
+                return result;
+            }
+
+            else     // convert to snake case -> this_is_a_variable->thisIsAVariable
+            {
+                var charArray = inputString.ToCharArray();
+
+                for (int i = 0; i < inputString.Length; i++)
+                {
+                    if (charArray[i] == '_' && i+1 < inputString.Length)
+                    {
+                        charArray[i+1] = char.ToUpper(charArray[i+1]);
+                    }
+                }
+                result = new string(charArray.Where(x => x != '_').ToArray());
+                return result;
+            }
+
+
+        }
 
         // Num of Decoding
 
-
         // CAT Pairs: HCF
-
-        // Snake to Pascal
-
-        // Pascal to Snake
-
-
-
     }
 
     }
